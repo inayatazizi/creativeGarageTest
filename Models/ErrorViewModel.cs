46dbc3013e63f018346683beb76d43db8932dc36
Inayat Ali <inayatazizi43@gmail.com>
2021-08-26 15:56:37 +0500

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JustTestingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
